d5e4bb96d0ecff721ac289ebb5168d58da8881f5
rholdy <rholdy@gmail.com>
2013-11-06 15:55:54 -0700

    Still workin on Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seva::Application.config.secret_token = '936a67cb2d7d77468f5298e460d65be29a9aa922e8dc731cca3638af5f4f1671fba2ac0e93d9273d92305c27cdc2ac75ffaea0314c8095852241fc567763df95' #ENV['SECRET_TOKEN']
