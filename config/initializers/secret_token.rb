7b0bbcdee3b87dd43c4a483c586d8b52667894ce
rholdy <rholdy@gmail.com>
2013-11-07 18:17:59 -0700

    Finally Fixed Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Seva::Application.config.secret_token = '936a67cb2d7d77468f5298e460d65be29a9aa922e8dc731cca3638af5f4f1671fba2ac0e93d9273d92305c27cdc2ac75ffaea0314c8095852241fc567763df95' #ENV['SECRET_TOKEN']
+Seva::Application.config.secret_token = ENV['SECRET_TOKEN']
